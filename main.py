d3a7d37bc29ed9121580a8b2a6133ce9da8a9f4d
Steven Lin <146241997+chickenwaddle77@users.noreply.github.com>
2023-11-24 14:20:25 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+
+df = pd.read_csv('data.csv')
+X = df.drop('fraud', axis=1)
+y = df['fraud']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Scale the features using StandardScaler so that both columns of the matrix are relative to each other
+scaler = StandardScaler()
+
+# Scale the training and test set so that the mean is 0? and S.D. is 1? (from i've read so far)
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Applying KNN model
+knn = KNeighborsClassifier(n_neighbors=4)
+knn.fit(X_train, y_train)
+
+# Make prediction using the X set of data, the output is y_pred because we want to know the y set (0 or 1)
+y_pred = knn.predict(X_test)
+
+# Compare what the actual y value is with the prediction for all values in the prediction set
+accuracy = accuracy_score(y_test, y_pred)
+print(y_test)
+print("Accuracy:", round(accuracy, 2))
+
+sns.scatterplot(x=df['dist_from_home'],y=df['purchase_price_ratio'], hue=df['fraud'])
+
+
+
+
