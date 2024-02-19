7795e7622ea4c7a9f44fa7a5830a1a38ff5f6cec
Steven Lin <146241997+chickenwaddle77@users.noreply.github.com>
2024-02-19 00:06:42 -0500

    Add files via upload

--- knn_new.py
+++ knn_new.py
@@ -1,26 +1,83 @@
 import numpy as np
 
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
+X = np.load("train_imgs.npy")
+y = np.load("train_labels.npy")
 
-img_train = np.load("train_imgs.npy")
-y_train = np.load("train_labels.npy")
+img_train, img_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=42, stratify=y)
 
 num_samples, height, width, channels = img_train.shape
-img_flattened = img_train.reshape(num_samples, height * width * channels)
-print(img_flattened.reshape((num_samples, height, width, channels)) == img_train)
+# print(img_train.shape, y_train.shape)
+img_train = img_train.reshape(num_samples, height * width * channels)
+# print(img_train.shape, y_train.shape, img_train[:, 0].shape, img_train[:, 1].shape)
+train_score = {}
+test_score = {}
 
-print(img_flattened.shape, img_train.shape, y_train.shape)
-print(img_flattened)
+n_neighbors = 3
+
+knn = KNeighborsClassifier(n_neighbors)
+knn.fit(img_train, y_train)
+acc = knn.score(img_train, y_train)
+print(acc * 100)
+
+
+# weights???? try that
+# neighbors vs accuracy
+
+
+# train_preds = knn.predict(img_train)
+
+# mse = mean_squared_error(y_train, train_preds)
+# rmse = sqrt(mse)
+#
+# print(rmse)
+
+# cmap = sns.cubehelix_palette(as_cmap=True)  # some colour stuff???
+# f, ax = plt.subplots()  # what is f and ax
+# points = ax.scatter(img_train[:, 0], img_train[:, 1], c=train_preds, s=50, cmap=cmap)
+# # x, y, c = gradient colourbar, cmap = size of dots
+#
+# f.colorbar(points)
+# plt.show()
+
+
+
+
+
+
+
+
+# train_score.update({neighbor: knn.score(img_train, y_train)})
+# test_score.update({neighbor: knn.score(img_test, y_test)})
+
+# print(train_score)
+# print(test_score)
+# plt.plot(n_neighbors, train_score, label="Train Accuracy")
+# plt.plot(n_neighbors, test_score, label="Test Accuracy")
+# plt.xlabel("Number Of Neighbors")
+# plt.ylabel("Accuracy")
+# plt.title("KNN: Varying number of Neighbors")
+# plt.legend()
+# plt.xlim(0, 33)
+# plt.ylim(0.60, 0.90)
+# plt.grid()
+# plt.show()
+#
+# print(img_flattened.reshape((num_samples, height, width, channels)) == img_train)
+#
+# print(img_flattened.shape, img_train.shape, y_train.shape)
+# print(img_flattened)
 
 # for sets in img_train:
 #     print(sets[0])
 #     knn = KNeighborsClassifier(n_neighbors=4)
 #     knn.fit(sets, y_train)
-
-knn = KNeighborsClassifier(n_neighbors=4)
-knn.fit(img_flattened, y_train)
+#
+# knn = KNeighborsClassifier(n_neighbors=4)
+# knn.fit(img_flattened, y_train)
--- main.py
+++ main.py
@@ -8,33 +8,33 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import seaborn as sns
 
-df = pd.read_csv('data.csv')
-X = df.drop('fraud', axis=1)
-y = df['fraud']
+# df = pd.read_csv('data.csv')
+# X = df.drop('fraud', axis=1)
+# y = df['fraud']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+img_train = np.load("train_imgs.npy")
+y_train = np.load("train_labels.npy")
+
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Scale the features using StandardScaler so that both columns of the matrix are relative to each other
 scaler = StandardScaler()
 
 # Scale the training and test set so that the mean is 0? and S.D. is 1? (from i've read so far)
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X_train = scaler.fit_transform(img_train)
+# X_test = scaler.transform(X_test)
 
 # Applying KNN model
 knn = KNeighborsClassifier(n_neighbors=4)
-knn.fit(X_train, y_train)
+knn.fit(img_train, y_train)
 
 # Make prediction using the X set of data, the output is y_pred because we want to know the y set (0 or 1)
-y_pred = knn.predict(X_test)
+# y_pred = knn.predict(X_test)
 
 # Compare what the actual y value is with the prediction for all values in the prediction set
-accuracy = accuracy_score(y_test, y_pred)
-print(y_test)
-print("Accuracy:", round(accuracy, 2))
+# accuracy = accuracy_score(y_test, y_pred)
+# print(y_test)
+# print("Accuracy:", round(accuracy, 2))
 
-sns.scatterplot(x=df['dist_from_home'],y=df['purchase_price_ratio'], hue=df['fraud'])
-
-
-
-
+sns.scatterplot(x=df['signature'],y=df['label'], hue=df['fraud'])
